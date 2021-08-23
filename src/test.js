ad7bffc4afb8f38af122fbf27ee17d49c44cb80b
ophélie <ophelieleleu@yahoo.fr>
2021-08-23 14:44:41 +0200

    jeu01N1 sans données de temps

--- src/test.js
+++ src/test.js
@@ -1,28 +1,37 @@
+import React, { useCallback, useState, useEffect } from 'react'
+import '../StyleTouchGameLevelOne.css'
 
-import React, { useCallback, useState } from 'react';
-import './App.css';
+function TouchGameLevelOne() {
 
-function App() {
-
-  const [size, setSize] = useState({ radius: 0 });
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-
-
+  const [size, setSize] = useState(0)
+  const [position, setPosition] = useState({ x: null, y: null })
+  
+  //recupere le premier click pour affi
   const mouseClick = useCallback((event) => {
-    setSize({ radius: size.radius + 40 });
-    setPosition({ x: event.clientX, y: event.clientY });
-  },[setSize, setPosition, size])
+    setSize(size + 40)
+    if (position.x === null) {
+      setPosition({ x: event.clientX, y: event.clientY })
+    }
+  }, [setSize, setPosition, size, position])
 
-  console.log("position", position)
+  console.log(position.x, position.y)
+
   return (
-    <div className="board" onClick={(event) => mouseClick(event)}
-    >
-     
-      <svg viewBox="0 0 600 900" fill="skyblue">
-        <circle cx={position.x} cy={position.y} r={size.radius} />
-      </svg>
+    <div className="yellow-board">
+      <div className="board" onClick={(event) => mouseClick(event)}>
+        <div style={{
+          background: 'skyblue',
+          position: 'fixed',
+          borderRadius: '50%',
+          top: position.y - size / 2,
+          left: position.x - size / 2,
+          width: size,
+          height: size,
+        }}>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default App
+export default TouchGameLevelOne

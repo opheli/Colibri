efd3fc5bc86d994332edc37687a3aaf645d66955
ophélie <ophelieleleu@yahoo.fr>
2021-08-25 13:31:34 +0200

    jeu 01 02 en cours

--- src/components/TouchGameLevelOne.jsx
+++ src/components/TouchGameLevelOne.jsx
@@ -1,18 +1,14 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 import '../StyleTouchGame.css'
 
-function TouchGameLevelOne() {
+function TouchGameLevelOne () {
 
   const BLACK_BOARD_SIZE = 1700
   const blackboard = useRef(null)
   const [size, setSize] = useState(0)
   const [mousePosition, setMousePosition] = useState({ x: null, y: null })
+  const [dcrb, setDcrb] = useState({ x: null, y: null })
   const [mouseDown, setMouseDown] = useState(false)
-
-  const calculX = window.innerWidth / 2 - mousePosition.x
-  const calculY =  window.innerHeight / 2 - mousePosition.y
-  const distance = Math.sqrt((calculX * calculX) + (calculY * calculY))
-  const rayonMax = distance + BLACK_BOARD_SIZE / 2
 
   const endGame = () => {
     console.log('fin jeu', endGame)
@@ -20,12 +16,23 @@ function TouchGameLevelOne() {
   }
 
   const getDotBigger = useCallback(() => {
-    if (size > rayonMax*2) {
+    const documentCenterX = window.innerWidth / 2
+    const documentCenterY = window.innerHeight / 2
+    const b = blackboard.current.getBoundingClientRect()
+    const documentCenterXreferentialBackboad = documentCenterX - b.x
+    const documentCenterYreferentialBackboad = documentCenterY - b.y
+    setDcrb({ x: documentCenterXreferentialBackboad, y: documentCenterYreferentialBackboad })
+    const calculX = documentCenterXreferentialBackboad - mousePosition.x
+    const calculY = documentCenterYreferentialBackboad - mousePosition.y
+    const distance = Math.sqrt((calculX * calculX) + (calculY * calculY))
+    const rayonMax = distance + BLACK_BOARD_SIZE / 2
+    setSize(size + 80)
+
+    console.info(size, rayonMax * 2)
+    if (size > rayonMax * 2) {
       endGame()
-    } else {
-      setSize(size + 80)
     }
-  }, [setSize, size, rayonMax, endGame])
+  }, [setSize, size, endGame, mousePosition, BLACK_BOARD_SIZE])
 
   const initDot = useCallback((event) => {
     if (mousePosition.x === null) {
@@ -50,11 +57,12 @@ function TouchGameLevelOne() {
 
 
   return (
-    <div className="yellow-board">
+    <div className="yellow-board"
+      onMouseUp={() => setMouseDown(false)}>
       <div
         className="black-board"
         onMouseDown={initDot}
-        onMouseUp={() => setMouseDown(false)}
+
         ref={blackboard}
         style={{ width: BLACK_BOARD_SIZE }}
       >
